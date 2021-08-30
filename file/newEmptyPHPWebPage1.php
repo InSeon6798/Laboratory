1628cba74f21762a363f039721a026b16a8f9562
InSeon6798 <dlstjs316@naver.com>
2021-08-30 17:13:15 +0900

    update

--- file/newEmptyPHPWebPage1.php
+++ file/newEmptyPHPWebPage1.php
@@ -1,6 +1,16 @@
-<!doctype html>
-<html lang="ko">
 
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        <h1>Hello!</h1>
+    </body>
+</html>
+
+
+<!-- comment --><!--<!doctype html>
+<html lang="ko">
     <head>
         <meta charset="utf-8">
         <title>Employees</title>
@@ -33,6 +43,7 @@
 
 
                 <?php
+                echo "Submit Clicked"
                 /* $title = $_POST['title'];
                 $file = $_FILE['file'];
 
@@ -55,48 +66,8 @@
                 } else {
                     echo "Error: " . $jb_sql . "<br>" . mysqli_error($jb_conn);
                 }*/
-    $currentDirectory = getcwd();
-    $uploadDirectory = "/uploads/";
-
-    $errors = []; // Store errors here
-
-    $fileExtensionsAllowed = ['jpeg','jpg','png']; // These will be the only file extensions allowed 
-
-    $fileName = $_FILES['the_file']['name'];
-    $fileSize = $_FILES['the_file']['size'];
-    $fileTmpName  = $_FILES['the_file']['tmp_name'];
-    $fileType = $_FILES['the_file']['type'];
-    $fileExtension = strtolower(end(explode('.',$fileName)));
-
-    $uploadPath = $currentDirectory . $uploadDirectory . basename($fileName); 
-
-    if (isset($_POST['submit'])) {
-
-      if (! in_array($fileExtension,$fileExtensionsAllowed)) {
-        $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
-      }
-
-      if ($fileSize > 4000000) {
-        $errors[] = "File exceeds maximum size (4MB)";
-      }
-
-      if (empty($errors)) {
-        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
-
-        if ($didUpload) {
-          echo "The file " . basename($fileName) . " has been uploaded";
-        } else {
-          echo "An error occurred. Please contact the administrator.";
-        }
-      } else {
-        foreach ($errors as $error) {
-          echo $error . "These are the errors" . "\n";
-        }
-      }
-
-    }
                 ?>
             </tbody>
         </table>
     </body>
-</html>
+</html>-->
